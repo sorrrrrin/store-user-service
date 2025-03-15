5e47a37e05638f2b3f60d16dbcd19d283a0fde97
sorrrrrin <46755684+sorrrrrin@users.noreply.github.com>
2025-03-15 07:58:00 +0200

    Added security permissions on endpoints

--- src/main/java/com/store/user/security/SecurityConfig.java
+++ src/main/java/com/store/user/security/SecurityConfig.java
@@ -1,10 +1,10 @@
-package com.store.user.config;
+package com.store.user.security;
 
-import com.store.user.security.JwtAuthenticationFilter;
 import com.store.user.utils.Constants;
 import com.store.user.utils.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +20,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    public SecurityConfig(CustomAuthenticationEntryPoint customAuthenticationEntryPoint, CustomAccessDeniedHandler customAccessDeniedHandler) {
+        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+        this.customAccessDeniedHandler = customAccessDeniedHandler;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http
@@ -30,10 +38,16 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/api/user/users").permitAll()
+                        .requestMatchers(HttpMethod.GET,  "/api/user/users").hasAuthority(Constants.ROLE_ADMIN)
+                        .requestMatchers(HttpMethod.DELETE,"/api/user/users", "/api/user/users/all").hasAuthority(Constants.ROLE_ADMIN)
                         .requestMatchers("/api/auth/login", "/public/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/admin/**").hasAuthority(Constants.ROLE_ADMIN)
-                        .requestMatchers("/user/**").hasAnyAuthority(Constants.ROLE_CUSTOMER, Constants.ROLE_ADMIN)
+                        .requestMatchers("/api/**").hasAnyAuthority(Constants.ROLE_CUSTOMER, Constants.ROLE_ADMIN)
                         .anyRequest().authenticated()
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler)
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil()), UsernamePasswordAuthenticationFilter.class);
