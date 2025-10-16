1a38d660372ff696ae757ce831e63c1db519f30f
sorrrrrin <46755684+sorrrrrin@users.noreply.github.com>
2025-10-16 13:34:14 +0300

    - testing CI/CD workflow

--- src/main/java/com/store/user/services/UserService.java
+++ src/main/java/com/store/user/services/UserService.java
@@ -53,7 +53,7 @@ public class UserService implements UserDetailsService {
     }
 
     public List<UserDto> getAllUsers() {
-        log.debug("Getting all users from database.");
+        log.debug("Get all users.");
         return userRepository.findAll().stream().map(userMapper::userToUserDto).collect(Collectors.toList());
     }
 
